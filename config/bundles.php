eac6c005ee8060d480f3e570fde961514b52d4ae
Vlad Galushka <galushka.vp@gmail.com>
2024-07-15 14:38:58 +0700

    install packages

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
